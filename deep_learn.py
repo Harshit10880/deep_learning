205202ae0d4238ef623191091895fc883e1c4f00
Harshit10880 <harshit.cha10@gmail.com>
2026-06-03 23:54:11 +0530

    learning: understand perceptron, weights, bias, and activation function

---
+++ deep_learn.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import numpy as np
+
+X = np.array([[1], [2], [3], [4]], dtype=float)
+
+y = np.array([[2], [4], [6], [8]], dtype=float)
+
+model = Sequential([
+    Dense(1, input_shape=(1,))
+])
+
+model.compile(optimizer='sgd', loss='mse')
+
+model.fit(X, y, epochs=100, verbose=0)
+
+result = model.predict(np.array([[5.0]]), verbose=0)
+
+print("Prediction for 5:", result[0][0])
